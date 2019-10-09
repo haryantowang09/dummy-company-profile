14736be2a98537e75ff8e21f5ffafc5a92b5190e
Haryanto <haryantowang09@gmail.com>
2019-10-09 13:19:51 +0700

    Basic set up done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app"><img src="./assets/logo.png">
-    <a-button type="primary">Button></a-button>
-    <router-view/>
-  </div>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider
+      :trigger="null"
+      collapsible
+      v-model="collapsed"
+    >
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="()=> collapsed = !collapsed"
+        />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+        <BaseGenSet msg="This effort below should not take like two days. Assuming you are only making this. Because I did it like 2 hours from scratch."/>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseGenSet from './components/BaseGenSet.vue';
 
 export default {
   name: 'app',
+  data(){
+    return {
+      collapsed: false,
+    }
+  },
   components: {
-    HelloWorld
+    BaseGenSet
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#components-layout-demo-custom-trigger {
+  height: 100vh;
+}
+
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color .3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255,255,255,.2);
+  margin: 16px;
 }
 </style>
---
+++ src/components/BaseGenSet.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <a-alert :message=this.msg() type="info" closeText="x"/>
+
+    <!-- here is the show begin -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseGenSet',
+  props: {
+    msg: String
+  },
+  methods: {
+    showMessage () {
+      return this.msg || ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
